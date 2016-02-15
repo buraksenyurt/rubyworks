d5a91740688ef5afb6a1c041a6ec20a04a6691ac
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2016-02-15 09:36:41 +0200

    Create Serializer.rb

---
+++ lesson1/Serializer.rb
@@ -0,0 +1,17 @@
+module Serializer
+ 
+def self.serialize(data)
+ 
+puts "#{data} serilestiriliyor"
+ 
+end
+ 
+  
+ 
+def self.deserialize(data)
+ 
+puts "#{data} ters serilestiriliyor"
+ 
+end
+ 
+end
