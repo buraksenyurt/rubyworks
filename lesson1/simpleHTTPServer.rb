8fcfdddc7c6692d5353dc16dd55e383d661fe355
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2016-02-23 13:56:46 +0200

    Update simpleHTTPServer.rb

--- lesson1/simpleHTTPServer.rb
+++ lesson1/simpleHTTPServer.rb
@@ -4,17 +4,22 @@ require "socket" #TCPServer ve TCPSocket siniflari burada yer almakta
 
 server=TCPServer.new('localhost',8082)
 
-loop do
-	socket=server.accept
-	request=socket.gets #gelen talebin ilk satiri okunuyor
+begin
+
+while(session=server.accept)
+	request=session.gets #gelen talebin ilk satiri okunuyor
 	STDERR.puts request #log' lar console' a yazilacak
 	
 	response="<b1>Hello Rubyist!</b1><br/><i>This is xion control. Wellcome home</i>"
-	socket.print "HTTP/1.1 200 OK\r\n" +
+	session.print "HTTP/1.1 200 OK\r\n" +
 				"Content-Type: text/html\r\n" +
 				"Content-Length: #{response.bytesize}\r\n" +
 				"Connection: close\r\n"
-	socket.print "\r\n"	#HTTP protokolu geregi bir alt satira gecilmesi gerekiyor
-	socket.print response #asil body mesaji yazdiriliyor
-	socket.close #sokect baglantisi kapatiliyor
+	session.print "\r\n"	#HTTP protokolu geregi bir alt satira gecilmesi gerekiyor
+	session.print response #asil body mesaji yazdiriliyor
+	session.close #sokect baglantisi kapatiliyor
 end
+
+rescue Errno::EPIPE
+  STDERR.puts "Connection broke!"
+  end
