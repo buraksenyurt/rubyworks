38a69afc75843d85076a7c1ca1343cfdeebb079c
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2017-04-12 09:08:42 +0300

    Delete Find.rb

--- lesson1/Find.rb
+++
@@ -1,9 +0,0 @@
-#bir dosyadaki belli kritere uygun satirlari bulmak
-puts 'dosya adi?'
-fileName=gets
-puts 'aranan grup?'
-groupName=gets
-puts groupName
-songs=File.readlines('SomeAlbums.txt')
-matches=songs.select{|line| line =~ /'#{groupName}'/}
-puts matches 
